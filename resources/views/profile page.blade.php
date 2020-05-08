a84e6a27993f368d46cfd03bbf91684ddb50f828
mahmoud19221 <mahmoud19221168@gmail.com>
2020-05-08 23:34:34 +0200

    uploading profile view

---
+++ resources/views/profile page.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+		<title>profile page</title> 
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+
+	<div class="box-area">
+		<header>
+			<div class="wrapper">
+				<div class="logo">
+					<a href="#">mourithems</a>
+					
+				</div>
+				<nav>
+					<a href="#">home</a>
+					<a href="#">profile</a>
+					<a href="#">about</a>
+					<a href="#">contact</a>		
+				</nav>
+			</div>
+
+		</header>
+		
+	</div>
+
+	<div class="card-container">
+		<div class="upper-container">
+		
+		<div class="image-container">
+
+			<img src="../../public/css/profile.jpg" />
+			
+		</div>	
+		</div>
+		<div class="lower-container">
+			<div class="tables">
+<table style="width:70%">
+ 	 <tr>
+    <td>username</td>
+    <td>mahmoud mostafa</td>
+    </tr>
+    <tr>
+    <td>phone number</td>	
+    <td>01033211705</td>
+    </tr> 
+    <tr>
+    <td>email</td>
+    <td>mahmoud19221168@gmail.com</td>
+    </tr>
+    <tr>
+    <td>address</td>	
+    <td>elslam city</td>
+    </tr>
+    <tr>
+    <td>date of birth</td>	
+    <td>19-8-1998</td>
+    </tr>
+    
+   
+</table>
+			</div>
+			<div class="buttons">
+			<div class="button">
+				<a href="#" class="btn">View course</a>
+				
+			</div>
+
+			<div class="button1">
+				<a href="#" class="btn">Edite profile</a>
+			</div>
+			</div>
+		</div>
+	
+	</div>
+
+
+	</body>
+
+</html>
