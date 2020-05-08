f46a38f6982a34d76c09073938b39f365c86ef86
Mahmoud Nagah <Mahmoudnagah999@gmail.com>
2020-05-08 22:56:56 +0300

    uploading the layout

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-inverse ">
+    <div class="container">
+        <div class="navbar-header">
+            <button  type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Disabled</a></li>
+            </ul>
+        </div>
+    </div>
+  </nav>
