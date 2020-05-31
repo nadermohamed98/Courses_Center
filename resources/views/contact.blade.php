203267421c7151cdb2d0d5115fecc2962c411202
3nkr <3nkora@gmail.com>
2020-05-31 13:41:32 +0200

    edit

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,37 @@
-@extends('layouts.app')
+<div style="background-image:linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)), url('matrial/contact.jpg');background-size:cover;">
+    @extends('layouts.app')
+    
+    @section('content')
+    <html>
+        
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+            <title>Contact us</title>
+    
+            <!-- Fonts -->
+            <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+            <link href="css/welcom.css" rel="stylesheet" type="text/css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            <link rel="stylesheet" href="css/slideshow.css">
+            
+        </head>
+        <body>
+            <div class="full-height" style="color:white; font-family:Arial" >
+                <h1> We would love to hear from you! </h1>
+                <h2> To learn more about us, visit our website<br> and connect with us on social media: </h2>
+                <a href="https://www.facebook.com/"><span class="fa fa-facebook"></span></a><br>
+                <a href="https://www.youtube.com/"><span class="fa fa-youtube"></span></a><br>
+                <a href="https://www.twitter.com/"><span class="fa fa-twitter"></span></a><br>
+                <a href="https://www.google.com/"><span class="fa fa-google"></span></a><br>
+                <a href="https://www.linkedin.com/"><span class="fa fa-linkedin"></span></a><br>
+                <a href="https://www.instagram.com/"><span class="fa fa-instagram"></span></a>
 
-@section('content')
-hello world with contact
-@endsection
+            </div>
+        </body>
+    </div>
+    </html>
+    @endsection
+    
