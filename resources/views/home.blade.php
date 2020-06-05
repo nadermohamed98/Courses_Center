d86749f938eaa2b63e92e77ea81997c47608642b
3nkr <3nkora@gmail.com>
2020-06-05 10:52:04 +0200

    edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,20 +13,35 @@
         </head>
         <body>
             <div class="full-height" style="color:white; font-family:Arial" >
-                <div class="w3-content" style="max-width:400px">
+                <div class="w3-content" style="max-width:100%; padding-top:100px;">
                     <div class="mySlides w3-container w3-red">
-                      <h1><b>Did You Know?  1</b></h1>
+                      <h1><b>Welcom to CORSATAK  </b></h1>
+                      <img src="/matrial/Untitled-1.jpg" style="hight:40%; width:90%;">
+                      
                       
                     </div>
                     <div class="mySlides w3-container w3-red">
-                        <h1><b>Did You Know?  2</b></h1>
+                        <h1><b>Here we connect students with teachers </b></h1>
+                        <img src="/matrial/Untitled-2.jpg" style="hight:40%; width:90%;">
                         
                       </div>
                     <div class="mySlides w3-container w3-red">
-                      <h1><b>Did You Know?  3</b></h1>
+                      <h1><b>We offer many courses in some fields you might be interest at!</b></h1>
+                      
+                      <img src="/matrial/1.png" style="hight:30%; width:23%;">
+                      <img src="/matrial/2.png" style="hight:30%; width:23%;">
+                      <img src="/matrial/3.png" style="hight:30%; width:23%;">
+                      <img src="/matrial/4.png" style="hight:30%; width:23%;">
+                      <hr>
+                      <img src="/matrial/5.png" style="hight:20%; width:23%;">
+                      <img src="/matrial/6.png" style="hight:20%; width:23%;">
+                      <img src="/matrial/7.png" style="hight:20%; width:23%;">
+                      <img src="/matrial/8.jpg" style="hight:20%; width:23%;">
+                      
+                      
                       
                     </div>
-                  
+                    
                   <script>
                   var slideIndex = 0;
                   carousel();
@@ -40,7 +55,7 @@
                     slideIndex++;
                     if (slideIndex > x.length) {slideIndex = 1} 
                     x[slideIndex-1].style.display = "block"; 
-                    setTimeout(carousel, 2000); 
+                    setTimeout(carousel, 4000); 
                   }
                   </script>
                   
